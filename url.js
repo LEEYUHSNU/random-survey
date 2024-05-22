c76d5956f1f17f3500539bb00055f5c628ca8293
LEEYUHSNU <leemichael2002@gmail.com>
2024-05-22 12:25:21 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,14 +1,4 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
- */
-
-
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/AfgsBoj54KfXWs8o6',
+	'https://forms.gle/BqkAtWTdWqnzMQ5XA',
 ];
